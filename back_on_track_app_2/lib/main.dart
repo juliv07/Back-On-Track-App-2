b7c434177f3f89b2f19eb89e22f4031138b66d9d
juliv07 <julivinocur@gmail.com>
2024-07-30 10:24:50 -0300

    Agregué firebase

--- back_on_track_app_2/lib/main.dart
+++ back_on_track_app_2/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:back_on_track_app_2/core/app_router.dart';
+import 'package:back_on_track_app_2/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main()async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
